8f17b1159b9b2412d49354bffb9a1b1cc6e127d9
Aliu Abdul Manaf <aliu.manaf@amalitech.com>
2024-11-06 15:43:34 +0000

    resoved bugs in the edit button

--- second_lab/script.js
+++ second_lab/script.js
@@ -26,9 +26,9 @@ function renderTodos() {
 
   if (todos.length === 0) {
     todoList.innerHTML = "<p>No tasks available</p>";
-    sortButton.style.display = "none";
+    sortButton.style.display = "none";  
   } else {
-    sortButton.style.display = "block";
+    sortButton.style.display = "block";  
 
     todos.forEach((todo) => {
       const todoItem = document.createElement("div");
@@ -38,23 +38,24 @@ function renderTodos() {
       todoItem.innerHTML = `
           <div class='items-in-todo'>
             <div>
-              <strong style='text-align: center'>${todo.title}</strong> 
-              <p>${todo.description}</p>
-            </div> 
+              <div class='title_actions'>
+                <strong style='font-family: "Lucida Console", "Courier New", monospace; font-size: 20px;'>${todo.title}</strong> 
+                <div>
+                  <button style='border: none; color: #007bff' onclick="editTodo(${todo.id})"> <i class="fa-solid fa-edit" aria-hidden="true"></i>
+                </button>
+                <button style='border: none; color: red' onclick="deleteTodo(${todo.id})"><i class="fa-solid fa-trash" aria-hidden="true"></i>
+                </button>
+                </div>
+                </div>
+              <p style='margin-top: 20px;margin-bottom: 10px;'>${todo.description}</p>
+              </div> 
             <div class='action-duedate'>
-              <small>${todo.dueDate.toLocaleString()}</small>
+              <small style='font-size: 10px'>${todo.dueDate.toLocaleString()}</small>
               <div class='action-bottom'>
-                <button onclick="toggleComplete(${todo.id})">${
-                  todo.completed ? "Unmark" : "Complete"
+                <button style='background-color: rgb(64, 218, 64);color: white; border: none' onclick="toggleComplete(${todo.id})">${
+                  todo.completed ? "Unmark as complete" : "Mark as Complete"
                 }</button>
-                <button style='border: none; color: #007bff' onclick="editTodo(${
-                  todo.id
-                })"> <i class="fa-solid fa-edit" aria-hidden="true"></i>
-                </button>
-                <button style='border: none; color: red' onclick="deleteTodo(${
-                  todo.id
-                })"><i class="fa-solid fa-trash" aria-hidden="true"></i>
-                </button>
+                
               </div>
             </div>
           </div>
@@ -64,6 +65,20 @@ function renderTodos() {
   }
 }
 
+function sortTodos() {
+  const selectElement = document.getElementById('sort-select');
+  const selectedValue = selectElement.value;
+
+  if (selectedValue === '2') { 
+    todos.sort((a, b) => a.dueDate - b.dueDate); 
+    renderTodos();
+    console.log('Sorting by Due Date...');
+  } else {
+    console.log('No sorting selected.');
+  }
+}
+
+
 function clearForm() {
   document.getElementById("title").value = "";
   document.getElementById("description").value = "";
@@ -71,8 +86,15 @@ function clearForm() {
 }
 
 function deleteTodo(id) {
+ // const findToDelete = todos.find((t) => t.id === id);
+  //const confirmDelete = confirm(`Are you sure you want to delete ${findToDelete.title} from the list`)
+ /* if(confirmDelete){
   todos = todos.filter((todo) => todo.id !== id);
   renderTodos();
+}
+  */
+todos = todos.filter((todo) => todo.id !== id);
+renderTodos();
 }
 
 function editTodo(id) {
@@ -88,10 +110,5 @@ function editTodo(id) {
 function toggleComplete(id) {
   const todo = todos.find((todo) => todo.id === id);
   todo.completed = !todo.completed;
-  renderTodos();
-}
-
-function sortTodos() {
-  todos.sort((a, b) => a.dueDate - b.dueDate);
   renderTodos();
 }
