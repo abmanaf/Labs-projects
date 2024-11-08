c0fe342a6f695d3dd4b1e38dfff36e470982dead
abmanaf <aliu.manaf@amalitech.com>
2024-11-08 11:39:32 +0000

    I removed the close button image and it functionality

--- third_lab/script.js
+++ third_lab/script.js
@@ -4,33 +4,33 @@ const lightBoxContent = document.createElement("div");
 const lightBoxImage = document.createElement("img");
 const lightBoxPrev = document.createElement("div");
 const lightBoxNext = document.createElement("div");
-const lightBoxClose = document.createElement("div");
+//const lightBoxClose = document.createElement("div");
 
 lightBoxContainer.classList.add("lightbox");
 lightBoxContent.classList.add("lightbox-content");
 lightBoxPrev.classList.add("fa", "fa-angle-left", "lightbox-prev");
 lightBoxNext.classList.add("fa", "fa-angle-right", "lightbox-next");
-lightBoxClose.classList.add("fa", "fa-times", "lightbox-close");
+//lightBoxClose.classList.add("fa", "fa-times", "lightbox-close");
 
 lightBoxContainer.appendChild(lightBoxContent);
 lightBoxContent.appendChild(lightBoxImage);
 lightBoxContent.appendChild(lightBoxPrev);
 lightBoxContent.appendChild(lightBoxNext);
-lightBoxContent.appendChild(lightBoxClose);
+//lightBoxContent.appendChild(lightBoxClose);
 
 document.body.appendChild(lightBoxContainer);
 
 let index = 0;
 const images = [
-  { src: "../images/download.jpeg", caption: "Image 1" },
-  { src: "../images/image_2.jpeg", caption: "Image 2" },
-  { src: "../images/image_3.jpeg", caption: "Image 3" },
-  { src: "../images/image_4.jpeg", caption: "Image 4" },
-  { src: "../images/image_5.jpeg", caption: "Image 5" },
-  { src: "../images/image_6.jpeg", caption: "Image 6" },
-  { src: "../images/image_7.jpeg", caption: "Image 7" },
-  { src: "../images/image_8.jpeg", caption: "Image 8" },
-  { src: "../images/image_9.jpeg", caption: "Image 9" },
+  { src: "../images/football-trainer-teaching-his-pupils_23-2149707974.avif", caption: "Image 1" },
+  { src: "../images/engineer-cooperation-male-female-technician-maintenance-control-relay-robot-arm-system-welding-with-tablet-laptop-control-quality-operate-process-work-heavy-industry-40-manufacturing-fa.avif", caption: "Image 2" },
+  { src: "../images/full-length-woman-exercising-field_1048944-30351094.avif", caption: "Image 3" },
+  { src: "../images/mom-spending-time-with-kid-beach_23-2150924978.avif", caption: "Image 4" },
+  { src: "../images/mom-spending-time-with-kid-beach_23-2150924982.avif", caption: "Image 5" },
+  { src: "../images/mom-spending-time-with-kid-beach_23-2150924983.avif", caption: "Image 6" },
+  { src: "../images/portrait-shirtless-man-standing-beach_1048944-30315343.avif", caption: "Image 7" },
+  { src: "../images/water-polo-player-pool-with-swimming-equipment_23-2150893950.avif", caption: "Image 8" },
+  { src: "../images/medium-shot-women-with-delicious-food_23-2150168102.avif", caption: "Image 9" },
 ];
 const lightBoxCaption = document.createElement("div");
 lightBoxCaption.classList.add("lightbox-caption");
@@ -73,18 +73,17 @@ function nextImage() {
 }
 
 function closeLightBox(event) {
-  if (event.target === lightBoxContainer || event.target === lightBoxClose) {
+  if (event.target === lightBoxContainer) {
     lightBoxContainer.style.display = "none";
   }
 }
 
 lightBoxPrev.addEventListener("click", prevImage);
 lightBoxNext.addEventListener("click", nextImage);
-lightBoxClose.addEventListener("click", closeLightBox);
+// /lightBoxClose.addEventListener("click", closeLightBox);
 
 for (let i = 0; i < item_gallery.length; i++) {
   item_gallery[i].addEventListener("click", currentImage);
 }
 
-// This automatically close lightbox if the user clicks outside the content
 lightBoxContainer.addEventListener("click", closeLightBox);
