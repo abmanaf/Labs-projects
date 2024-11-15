9e820b1b8c79de701be7fb253c8910326353cd7c
abmanaf <aliu.manaf@amalitech.com>
2024-11-15 06:13:46 +0000

    initialized variables

--- week2/time-master/time.js
+++ week2/time-master/time.js
@@ -1,54 +1,61 @@
 const date = new Date();
-const displayClock = document.querySelector('div');
-const toggleButton = document.getElementById('toggle-button');
+const displayClock = document.getElementById("display-clock");
+const toggleButton = document.getElementById("toggle-button");
+const timeZoneSelect = document.getElementById("time-zone");
+const dateTimeInput = document.getElementById("datetime-local");
+const setAlarmButton = document.getElementById("set-alarm-button");
+
+let alarmTime = null;
+let alarmSet = false;
 
 const clock = {
-    hour: date.getHours(),
-    minute: date.getMinutes(),
-    second: date.getSeconds(),
-    is24HourFormat: false,  
-    
-    updateTime() {
-        const now = new Date();
-        this.hour = now.getHours();
-        this.minute = now.getMinutes();
-        this.second = now.getSeconds();
-    },
+  hour: date.getHours(),
+  minute: date.getMinutes(),
+  second: date.getSeconds(),
+  is24HourFormat: false,
 
-    getFormattedTime() {
-        if (this.is24HourFormat) {
-            return `${this.hour}:${this.minute < 10 ? '0' + this.minute : this.minute}:${this.second < 10 ? '0' + this.second : this.second}`;
-        } else {
-            return this.get12HourTime();
-        }
-    },
+  updateTime() {
+    const now = new Date();
+    this.hour = now.getHours();
+    this.minute = now.getMinutes();
+    this.second = now.getSeconds();
+  },
 
-    get12HourTime() {
-        let ampm = this.hour >= 12 ? 'PM' : 'AM';
-        let hours = this.hour % 12;
-        hours = hours ? hours : 12;  
-        let minutes = this.minute < 10 ? '0' + this.minute : this.minute;
-        let seconds = this.second < 10 ? '0' + this.second : this.second;
-        return `${hours}:${minutes}:${seconds} ${ampm}`;
-    },
-
-    toggleTimeFormat() {
-        this.is24HourFormat = !this.is24HourFormat;
-        displayClock.innerHTML = this.getFormattedTime();  
-    },
-
-    startClock() {
-        setInterval(() => {
-            this.updateTime();
-            displayClock.innerHTML = this.getFormattedTime();  
-        }, 1000);
+  getFormattedTime() {
+    if (this.is24HourFormat) {
+      return `${this.hour}:${
+        this.minute < 10 ? "0" + this.minute : this.minute
+      }:${this.second < 10 ? "0" + this.second : this.second}`;
+    } else {
+      return this.get12HourTime();
     }
+  },
+
+  get12HourTime() {
+    let ampm = this.hour >= 12 ? "PM" : "AM";
+    let hours = this.hour % 12;
+    hours = hours ? hours : 12;
+    let minutes = this.minute < 10 ? "0" + this.minute : this.minute;
+    let seconds = this.second < 10 ? "0" + this.second : this.second;
+    return `${hours}:${minutes}:${seconds} ${ampm}`;
+  },
+
+  toggleTimeFormat() {
+    this.is24HourFormat = !this.is24HourFormat;
+    displayClock.innerHTML = this.getFormattedTime();
+  },
+
+  startClock() {
+    setInterval(() => {
+      this.updateTime();
+      displayClock.innerHTML = this.getFormattedTime();
+    }, 1000);
+  },
 };
 
 displayClock.innerHTML = clock.getFormattedTime();
 clock.startClock();
 
-toggleButton.addEventListener('click', () => {
-    clock.toggleTimeFormat();  
+toggleButton.addEventListener("click", () => {
+  clock.toggleTimeFormat();
 });
-
