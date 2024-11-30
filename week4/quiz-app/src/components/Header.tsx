d647cbddfe561a7b2f5a30fa3466d591adf140f8
abmanaf <aliu.manaf@amalitech.com>
2024-11-30 13:56:04 +0000

    header: worked on the header

--- week4/quiz-app/src/components/Header.tsx
+++ week4/quiz-app/src/components/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { QuizProps } from './Quiz';
 
-const Header: React.FC<QuizProps> = ({quizData, onRestart}) => {
-    return(
-        <div className='header-image'>
-            <img src={quizData.icon} alt={`${quizData.title} icon`} />
-            <span>{quizData.title}</span>
-        </div>
-    )
-}
+const Header: React.FC<QuizProps> = ({ quizData, onRestart }) => {
+    if (!quizData) {
+      return null; 
+    }
+  
+    return (
+      <div className="header-image">
+        <img src={quizData.icon} alt={`${quizData.title} icon`} />
+        <span>{quizData.title}</span>
+      </div>
+    );
+  };
+  
 export default Header;
