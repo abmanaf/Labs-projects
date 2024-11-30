f8d0ab064e35cbce6c4aafb3062f68f642a355cc
abmanaf <aliu.manaf@amalitech.com>
2024-11-30 13:54:40 +0000

    types: initialized types correctly

--- week4/quiz-app/src/types/quiz.types.ts
+++ week4/quiz-app/src/types/quiz.types.ts
@@ -1,24 +1,24 @@
 export interface QuizSubject {
-    quizzes: string;
-    icon: string;
-    color: string;
-  }
-  
-  export interface QuizQuestion {
-    id: number;
-    question: string;
-    options: string[];
-    answer: string;
-  }
-  
-  export interface QuizData {
-    quizzes: string;
-    title: string;
-    icon: string;
-    questions: QuizQuestion[];
-  }
-  
-  export interface ThemeContextType {
-    isDarkMode: boolean;
-    toggleTheme: () => void;
-  }
+  quizzes: string;
+  icon: string;
+  color: string;
+}
+
+export interface QuizQuestion {
+  id: number;
+  question: string;
+  options: string[];
+  answer: string;
+}
+
+export interface QuizData {
+  quizzes: string;
+  title: string;
+  icon: string;
+  questions: QuizQuestion[];
+}
+
+export interface ThemeContextType {
+  isDarkMode: boolean;
+  toggleTheme: () => void;
+}
