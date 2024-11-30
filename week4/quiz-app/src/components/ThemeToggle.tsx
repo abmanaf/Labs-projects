580146198fa04186dbbbb15a65579d391933441b
abmanaf <aliu.manaf@amalitech.com>
2024-11-30 13:57:34 +0000

    theme: worked on the theme toggling

--- week4/quiz-app/src/components/ThemeToggle.tsx
+++ week4/quiz-app/src/components/ThemeToggle.tsx
@@ -6,18 +6,9 @@ const ThemeToggle: React.FC = () => {
 
   return (
     <div style={{display: 'flex', gap: '0.5em'}}>
-      {/* 
-    <button 
-      className="theme-toggle"
-      onClick={toggleTheme}
-      aria-label={`Switch to ${isDarkMode ? 'light' : 'dark'} mode`}
-    >
-      {isDarkMode ? '☀️' : '🌙'}
-    </button>
-    */}
     <img src="src/assets/images/sunny.svg" alt="sunny" />
     <img className="theme-toggle"
-    onClick={toggleTheme} src="src/assets/images/toggle.svg" alt="toggle" />
+      onClick={toggleTheme} src="src/assets/images/toggle.svg" alt="toggle" />
     <img src="src/assets/images/moon.svg" alt="moon" />
 
     </div>
