24bbf746fdaf52c00037e95234f7f69180bb84ee
abmanaf <aliu.manaf@amalitech.com>
2024-12-02 17:42:49 +0000

    refactor:extracted an svg file into a separate component

--- week4/quiz-app/src/components/ThemeToggle.tsx
+++ week4/quiz-app/src/components/ThemeToggle.tsx
@@ -3,39 +3,17 @@ import darkmode from '/assets/images/darkmode-moon.svg';
 import moon from '/assets/images/moon.svg';
 import darkModeSun from '/assets/images/darkmode-sun.svg';
 import sun from '/assets/images/sunny.svg'
+import ToggleIcon from './ToggleIcon';
 
 
 const ThemeToggle = () => {
-  const { isDarkMode, toggleTheme } = useTheme();
+  const { isDarkMode } = useTheme();
 
   return (
-    <div style={{ display: 'flex', gap: '0.5em' }}>
+    <div className='toggle-container'>
       {!isDarkMode? <img  src={sun} alt="sunny" />: <img src={darkModeSun} alt="darkmode-sun"/>}
-      <svg
-        className="theme-toggle"
-        style={{ cursor: 'pointer' }}
-        onClick={toggleTheme}
-        width="48"
-        height="28"
-        viewBox="0 0 48 28"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect
-          width="48"
-          height="28"
-          rx="14"
-          fill={isDarkMode ? "#A729F5" : "#A729F5"} 
-        />
-        <circle
-          cx={isDarkMode ? "34" : "14"} 
-          cy="14"
-          r="10"
-          fill="white"
-        />
-      </svg>
+      <ToggleIcon />
       {!isDarkMode? <img  src={moon} alt="moon" />: <img src={darkmode} alt="darkmode-moon"/>}
-
     </div>
   );
 };
